b38bdaf2ec91c0f17439e4b1f802bb1d15772967
MatMasIt <34745996+MatMasIt@users.noreply.github.com>
2021-05-22 15:35:28 +0200

    Create time.sh

---
+++ time.sh
@@ -0,0 +1,2 @@
+# !/bin/sh
+dialog  --yes-label "I Understand" --no-label "remain ignorant" --backtitle "Unfortunately, the clock is ticking" --yesno "the hours are going by. The past increases, the future recedes. Possibilities decreasing, regrets mounting\n\nDo you understand?" 10 100
